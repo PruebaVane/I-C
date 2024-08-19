939fa9019f21d83dd0bce640d7036bb951ef73f7
PruebaVane <gimenezv0711@gmail.com>
2024-08-19 19:50:14 -0300

    Otro cambio mas

--- src/app.py
+++ src/app.py
@@ -166,5 +166,5 @@ if __name__ == "__main__":
         time.sleep(1)
         
 print("Hola Mundo - Hellow world!!!")
-print("PROFESOR!!!")
 print("nuevo intento de crear contetnedor")
+print("Ultimo intento del día de hoy")
